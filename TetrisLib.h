34a0f91b5968d3370ff9bea66ea46a557d8db28c
avo2000-mail-ru <108999278+avo2000-mail-ru@users.noreply.github.com>
2023-07-10 23:45:24 +0300

    Update TetrisLib.h

--- TetrisLib.h
+++ TetrisLib.h
@@ -8,26 +8,5 @@
 #ifndef _id_1932c18a_3257_4711_9925_988c3cbfd57f
 #define _id_1932c18a_3257_4711_9925_988c3cbfd57f
 
-#ifdef _MSC_VER
-  #ifdef _DLL
-    #ifdef __cplusplus
-      #define _EXPORTS(type) extern "C" __declspec(dllexport) type __stdcall
-    #else
-      #define _EXPORTS(type) __declspec(dllexport) type __stdcall
-    #endif
-  #else
-    #ifdef __cplusplus
-      #define _EXPORTS(type) extern "C" __declspec(dllimport) type __stdcall
-    #else
-      #define _EXPORTS(type) __declspec(dllimport) type __stdcall
-    #endif
-  #endif
-#else
-  #ifdef __cplusplus
-    #define _EXPORTS(type) extern "C" type
-  #else
-    #define _EXPORTS(type) type
-  #endif
-#endif
 
 #endif /* _id_1932c18a_3257_4711_9925_988c3cbfd57f */
